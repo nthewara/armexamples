ac63c9a2ca9fa9c47cadfb3e567f8a18e6ee9c1c
Nirmal Thewarathanthri <nirmal@live.com>
2019-03-24 23:08:25 +0800

    added random name generation for azure resources

--- Network/azureFirewalls/AzDeploy.ps1
+++ Network/azureFirewalls/AzDeploy.ps1
@@ -1,13 +1,22 @@
-$resourceGroupName = ""
-$azfirewallname = "" 
-$vnetname = ""
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory=$true)] $vnetname
+)
+
+#Variables
+$Randomstring = -join ((97..122) | Get-Random -Count 8 | % {[char]$_})
+
+#Azuredeployment parameters
+$azfirewallname = "azfw-"+$Randomstring 
 $publicIpAddressName = $azfirewallname+"-pip01"
-$templateFilePath = ""
-$parameterfile = ""
+
+#Template location (Github)
+$templateFilePath = "https://raw.githubusercontent.com/nthewara/armexamples/master/Network/azureFirewalls/firewall.json"
+$parameterfile = "https://raw.githubusercontent.com/nthewara/armexamples/master/Network/azureFirewalls/parameters.firewall.json"
 
 #Validate
-Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $paramfile -azureFirewallName $azfirewallname -virtualNetworkName $vnetname -publicipaddressname $publicIpAddressName
+#Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateFilePath -TemplateParameterUri $paramfile -azureFirewallName $azfirewallname -virtualNetworkName $vnetname -publicipaddressname $publicIpAddressName
 
 #deploy
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $paramfile -azureFirewallName $azfirewallname -virtualNetworkName $vnetname -publicipaddressname $publicIpAddressName
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateFilePath -azureFirewallName $azfirewallname -virtualNetworkName $vnetname -publicipaddressname $publicIpAddressName
 
