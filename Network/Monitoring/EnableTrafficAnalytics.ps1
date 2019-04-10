72b0ddfd3503d02c457c1af562cc0a8b6bd58bdf
Nirmal Thewarathanthri <nirmal@live.com>
2019-04-11 04:32:52 +0800

    added loop to apply against multiple nsgs

--- Network/Monitoring/EnableTrafficAnalytics.ps1
+++ Network/Monitoring/EnableTrafficAnalytics.ps1
@@ -26,4 +26,16 @@ $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $nsgrgname -Name $nsgname
 #set Flowlog configuration 
 Set-AzNetworkWatcherConfigFlowLog -FormatVersion 2 -FormatType Json -NetworkWatcher $NW -TargetResourceId $nsg.Id -EnableFlowLog $true -StorageAccountId $storageID -EnableTrafficAnalytics -Workspace $workspace -RetentionInDays 30 
 
+#apply to multiple NSGs 
+$Listofnsgs = Get-AzNetworkSecurityGroup
+ForEach ($nsgname in $listofnsgs) {
+    try {
+        Write-Output "Starting to process nsg = $nsgname" 
+        Set-AzNetworkWatcherConfigFlowLog -FormatVersion 2 -FormatType Json -NetworkWatcher $NW -TargetResourceId $nsg.Id -EnableFlowLog $true -StorageAccountId $storageID -EnableTrafficAnalytics -Workspace $workspace -RetentionInDays 30 
+    } Catch {
+        $errorDetails = $_.Exception.Message
+        $ErrorMessages += "Error: $errorDetails `n"
+    }
+    
+    }
 
